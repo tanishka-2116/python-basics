dde11e509ea9ba8f14f7bb945cb09af4c7ca84e0
tanishka-2116 <tanishkadhumal2006@gmail.com>
2026-01-08 18:44:51 +0530

    cal_math.py

---
+++ cal_math.py
@@ -0,0 +1,13 @@
+# Simple math operations using user input
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Cannot divide by zero")
